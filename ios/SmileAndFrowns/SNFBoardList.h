1db6738b2a47b00f35cb1bf1e310ff1aab401369
pixelrevision <pixelrevision@gmail.com>
2015-10-29 16:27:19 -0400

    board list updates

--- ios/SmileAndFrowns/SNFBoardList.h
+++ ios/SmileAndFrowns/SNFBoardList.h
@@ -1,8 +1,10 @@
 #import <UIKit/UIKit.h>
 
-@interface SNFBoardList : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface SNFBoardList : UIViewController <UITableViewDelegate, UITableViewDataSource>{
+	NSArray *_boards;
+}
 
-@property (weak) IBOutlet UITableView *boardstable;
+@property (weak) IBOutlet UITableView *boardsTable;
 @property (weak) IBOutlet UISegmentedControl *filterControl;
 @property (weak) IBOutlet UIButton *searchButton;
 @property (weak) IBOutlet UIButton *purchaseButton;
