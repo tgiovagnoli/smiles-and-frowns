8809dad9286d2ca9d0066c5823cc66d3f140d29e
Aaron Smith <gngrwzrd@gmail.com>
2015-10-31 16:04:45 -0700

    Added gender picker to signup

--- ios/SmileAndFrowns/SNFCreateAccount.h
+++ ios/SmileAndFrowns/SNFCreateAccount.h
@@ -2,12 +2,15 @@
 #import <UIKit/UIKit.h>
 #import "MBProgressHUD.h"
 
-@interface SNFCreateAccount : UIViewController
+@interface SNFCreateAccount : UIViewController <UIPickerViewDataSource,UIPickerViewDelegate>
 
 @property UIViewController * nextViewController;
 
+@property IBOutlet UIView * pickerviewContainer;
+@property (weak) IBOutlet UIPickerView * pickerView;
+
 @property IBOutlet UIView * formView;
-@property IBOutlet UIScrollView * scrollView;
+@property (weak) IBOutlet UIScrollView * scrollView;
 @property IBOutlet NSLayoutConstraint * scrollViewBottom;
 
 @property (weak) IBOutlet UITextField * email;
@@ -15,5 +18,8 @@
 @property (weak) IBOutlet UITextField * lastname;
 @property (weak) IBOutlet UITextField * password;
 @property (weak) IBOutlet UITextField * passwordConfirm;
+@property (weak) IBOutlet UITextField * gender;
+@property (weak) IBOutlet UIButton * genderOverlay;
+@property (weak) IBOutlet UITextField * age;
 
 @end
--- ios/SmileAndFrowns/SNFCreateAccount.m
+++ ios/SmileAndFrowns/SNFCreateAccount.m
@@ -6,9 +6,12 @@
 #import "SNFModel.h"
 #import "UIView+LayoutHelpers.h"
 #import "SNFLogin.h"
+#import "NSTimer+Blocks.h"
 
 @interface SNFCreateAccount ()
 @property BOOL firstlayout;
+@property NSArray * genders;
+@property NSTimer * pickerTimer;
 @end
 
 @implementation SNFCreateAccount
@@ -16,6 +19,9 @@
 - (void) viewDidLoad {
 	[super viewDidLoad];
 	self.firstlayout = true;
+	self.genders = @[@"--------",@"Male",@"Female"];
+	self.pickerView.delegate = self;
+	[self.genderOverlay setTitle:@"" forState:UIControlStateNormal];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 }
@@ -41,7 +47,7 @@
 		return;
 	}
 	self.scrollViewBottom.constant = keyboardFrameEnd.size.height;
-	self.formView.height -= 100;
+	self.formView.height = 460;
 	self.scrollView.contentSize = CGSizeMake(self.scrollView.width,self.formView.height);
 }
 
@@ -50,8 +56,50 @@
 		return;
 	}
 	self.scrollViewBottom.constant = 0;
-	self.formView.height += 100;
 	self.scrollView.contentSize = CGSizeMake(self.scrollView.width,self.formView.height);
+	[NSTimer scheduledTimerWithTimeInterval:.2 block:^{
+		self.formView.height = self.scrollView.height;
+	} repeats:FALSE];
+}
+
+- (IBAction) onGender:(id)sender {
+	[self.view endEditing:TRUE];
+	self.pickerviewContainer.frame = self.view.bounds;
+	[self.pickerView reloadAllComponents];
+	
+	self.pickerviewContainer.alpha = 0;
+	[self.view addSubview:self.pickerviewContainer];
+	
+	UIViewAnimationOptions options = UIViewAnimationOptionCurveEaseInOut;
+	[UIView animateWithDuration:.25 delay:0 options:options animations:^{
+		self.pickerviewContainer.alpha = 1;
+	} completion:^(BOOL finished) {
+		
+	}];
+	
+}
+
+- (NSInteger) numberOfComponentsInPickerView:(UIPickerView *) pickerView {
+	return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+	return self.genders.count;
+}
+
+- (NSString *) pickerView:(UIPickerView *) pickerView titleForRow:(NSInteger)row forComponent:(NSInteger) component {
+	return [self.genders objectAtIndex:row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+	if(row == 0) {
+		return;
+	}
+	self.gender.text = [self.genders objectAtIndex:row];
+}
+
+- (IBAction) closePicker:(id)sender {
+	[self.pickerviewContainer removeFromSuperview];
 }
 
 - (IBAction) signup:(id) sender {
@@ -63,6 +111,8 @@
 		@"lastname":self.lastname.text,
 		@"password":self.password.text,
 		@"password_confirm":self.passwordConfirm.text,
+		@"gender":self.gender.text.lowercaseString,
+		@"age":self.age.text,
 	};
 	
 	[MBProgressHUD showHUDAddedTo:self.view animated:TRUE];
