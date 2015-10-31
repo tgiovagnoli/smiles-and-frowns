1be69cd9f8b65def3e16588d754f154680b7434c
Aaron Smith <gngrwzrd@gmail.com>
2015-10-31 10:48:31 -0700

    Added login persistence.

--- ios/SmileAndFrowns/SNFModel.h
+++ ios/SmileAndFrowns/SNFModel.h
@@ -4,12 +4,14 @@
 #import "SNFUser.h"
 #import "SNFUserSettings.h"
 
-@interface SNFModel : NSObject
+@interface SNFModel : NSObject {
+	SNFUser * _loggedInUser;
+}
 
-@property NSManagedObjectContext *managedObjectContext;
-@property (readonly) SNFConfig *config;
-@property (readonly) SNFUserSettings *userSettings;
-@property SNFUser *loggedInUser;
+@property NSManagedObjectContext * managedObjectContext;
+@property (readonly) SNFConfig * config;
+@property (readonly) SNFUserSettings * userSettings;
+@property (nonatomic) SNFUser * loggedInUser;
 @property NSString * pendingInviteCode;
 
 + (SNFModel *)sharedInstance;
--- ios/SmileAndFrowns/SNFModel.m
+++ ios/SmileAndFrowns/SNFModel.m
@@ -1,21 +1,54 @@
+
 #import "SNFModel.h"
+#import "SNFUser.h"
 
 static SNFModel *_instance;
 
 @implementation SNFModel
 
-+ (SNFModel *)sharedInstance{
-	if(!_instance){
++ (SNFModel *) sharedInstance {
+	if(!_instance) {
 		_instance = [[SNFModel alloc] init];
 	}
 	return _instance;
 }
 
-- (id)init{
+- (id) init {
 	self = [super init];
 	_config = [[SNFConfig alloc] init];
 	_userSettings = [[SNFUserSettings alloc] init];
 	return self;
+}
+
+- (void) setLoggedInUser:(SNFUser *) loggedInUser {
+	_loggedInUser = loggedInUser;
+	if(!loggedInUser) {
+		[[NSUserDefaults standardUserDefaults] removeObjectForKey:@"SNFModel.LoggedInUser"];
+	} else {
+		[[NSUserDefaults standardUserDefaults] setObject:loggedInUser.username forKey:@"SNFModel.LoggedInUser"];
+	}
+}
+
+- (SNFUser *) loggedInUser {
+	if(!_loggedInUser) {
+		NSString * username = [[NSUserDefaults standardUserDefaults] objectForKey:@"SNFModel.LoggedInUser"];
+		if(username) {
+			NSError * error = nil;
+			NSFetchRequest * fetchRequest = [[NSFetchRequest alloc] init];
+			NSEntityDescription * entity = [NSEntityDescription entityForName:@"SNFUser" inManagedObjectContext:[SNFModel sharedInstance].managedObjectContext];
+			[fetchRequest setEntity:entity];
+			[fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"(username = %@)",username]];
+			NSArray * fetchedObjects = [[SNFModel sharedInstance].managedObjectContext executeFetchRequest:fetchRequest error:&error];
+			if(error) {
+				NSLog(@"error making query: %@",error);
+				return nil;
+			}
+			if(fetchedObjects.count > 0) {
+				_loggedInUser = [fetchedObjects objectAtIndex:0];
+			}
+		}
+	}
+	return _loggedInUser;
 }
 
 @end
