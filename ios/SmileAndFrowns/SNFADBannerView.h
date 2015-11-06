2237705bc55df9f69329d1d4c998b7fe03c997ec
Aaron Smith <gngrwzrd@gmail.com>
2015-11-06 15:45:51 -0800

    Added remove ads purchases

---
+++ ios/SmileAndFrowns/SNFADBannerView.h
@@ -0,0 +1,13 @@
+//
+//  SNFADBannerView.h
+//  SmileAndFrowns
+//
+//  Created by Aaron Smith on 11/6/15.
+//  Copyright © 2015 apptitude. All rights reserved.
+//
+
+#import <iAd/iAd.h>
+
+@interface SNFADBannerView : ADBannerView
+
+@end
---
+++ ios/SmileAndFrowns/SNFADBannerView.m
@@ -0,0 +1,21 @@
+//
+//  SNFADBannerView.m
+//  SmileAndFrowns
+//
+//  Created by Aaron Smith on 11/6/15.
+//  Copyright © 2015 apptitude. All rights reserved.
+//
+
+#import "SNFADBannerView.h"
+
+@implementation SNFADBannerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
