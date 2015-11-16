8ac28e17ec4e3d40d53e7b60f707ebc800d63a19
Aaron Smith <gngrwzrd@gmail.com>
2015-11-16 11:39:59 -0800

    settings for mysql, commented out

---
+++ ios/SmileAndFrowns/SNFUUID.h
@@ -0,0 +1,13 @@
+//
+//  SNFUUID.h
+//  SmileAndFrowns
+//
+//  Created by Aaron Smith on 11/16/15.
+//  Copyright © 2015 apptitude. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SNFUUID : NSObject
+
+@end
