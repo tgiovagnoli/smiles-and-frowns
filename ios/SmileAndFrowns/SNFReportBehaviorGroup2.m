534ceae6632694a7b23e0ef49f39d91a88be49e4
Aaron Smith <gngrwzrd@gmail.com>
2016-02-12 08:10:44 -0800

    Remove dup notes

--- ios/SmileAndFrowns/SNFReportBehaviorGroup2.m
+++ ios/SmileAndFrowns/SNFReportBehaviorGroup2.m
@@ -14,10 +14,12 @@
 - (void) addSmile:(SNFSmile *) object; {
 	[self.objects addObject:object];
 	if(object.note && object.note.length > 0) {
-		if(self.notes.length < 1) {
-			[self.notes appendString:object.note];
-		} else {
-			[self.notes appendFormat:@", %@",object.note];
+		if(![self.notes containsString:object.note]) {
+			if(self.notes.length < 1) {
+				[self.notes appendString:object.note];
+			} else {
+				[self.notes appendFormat:@", %@",object.note];
+			}
 		}
 	}
 }
@@ -25,10 +27,12 @@
 - (void) addFrown:(SNFFrown *) object; {
 	[self.objects addObject:object];
 	if(object.note && object.note.length > 0) {
-		if(self.notes.length < 1) {
-			[self.notes appendString:object.note];
-		} else {
-			[self.notes appendFormat:@", %@",object.note];
+		if(![self.notes containsString:object.note]) {
+			if(self.notes.length < 1) {
+				[self.notes appendString:object.note];
+			} else {
+				[self.notes appendFormat:@", %@",object.note];
+			}
 		}
 	}
 }
